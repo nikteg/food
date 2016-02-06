75f3d97e2a496e7a0b8060a7d9c93ec913084a01
Niklas Tegnander <bipshark@gmail.com>
2016-02-06 01:44:17 +0100

    Error handling

--- index.js
+++ index.js
@@ -7,22 +7,28 @@ var app = express()
 
 app.use(express.static('public'))
 
-app.get('/restaurant/:restaurant', function (req, res) {
+app.get('/restaurant/:restaurant', function (req, res, next) {
   var restaurant = restaurants[req.params.restaurant]
 
-  if (!restaurant) return console.error(restaurant)
+  if (!restaurant) return next(new Error("Invalid restaurant"))
 
   request(restaurant.url, function (err, resp, body) {
-    if (err) return console.error(url, err)
-    if (resp.statusCode !== 200) return console.error(url, resp.statusCode)
+    if (err) return next(new Error("Request error"))
+    if (resp.statusCode !== 200) return next(new Error("Request not 200"))
 
     res.header("Content-Type", restaurant.format)
     res.send(body)
   })
 })
 
+// Error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.send(err.message)
+})
+
 var port = process.env.PORT || 3000
 
 app.listen(port, function () {
-  console.log("App listening on port 3000!");
+  console.log("App listening on port", port);
 })
