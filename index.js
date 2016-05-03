f300dec85c9f67c8d3fee37402a51c7505b6c55a
Niklas Tegnander <bipshark@gmail.com>
2016-05-03 23:01:52 +0200

    Don't serve public stuff on proxy

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var restaurants = require("./restaurants.json")
 
 var app = express()
 
-app.use(express.static('public'))
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
